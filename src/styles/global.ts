aac7b3a2b3487bc49e224305a78ee1d1a9861b1f
erickmarquesc <erick.marquesc@hotmail.com>
2022-09-21 15:37:18 -0300

    style: :lipstick: update UI

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,9 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
   };
 
   :focus{
@@ -17,9 +20,4 @@ export const GlobalStyle = createGlobalStyle`
     color: ${props => props.theme["gray-300"]};
   };
 
-  border-style, input-security, textarea, button{
-    font-family: 'Roboto', sans-serif;
-    font-weight: 400;
-    font-size: 1rem;
-  };
 `;
