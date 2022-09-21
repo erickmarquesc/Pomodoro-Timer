923d1781495ae9356f8dc5a6cbc4681456b4fb40
erickmarquesc <erick.marquesc@hotmail.com>
2022-09-20 22:55:19 -0300

    style: :lipstick: update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-
   return (
-    <h1>Pomodoro</h1>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Pomodoro</h1>
+      <GlobalStyle/>
+    </ThemeProvider>
+  );
 };
