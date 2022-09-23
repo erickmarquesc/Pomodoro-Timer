8c62236989bb5ccd7c071fb80e91d6b1f89133c4
erickmarquesc <erick.marquesc@hotmail.com>
2022-09-23 14:10:28 -0300

    refactor: :recycle: refactor

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,49 @@
+import { Play } from "phosphor-react";
+import { 
+  CountdownContainer, 
+  FormContainer, 
+  HomeContainer, 
+  MinutesAmountInput, 
+  Separator, 
+  StartCountdownButton, 
+  TaskInput 
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+
+          <label>Vou trabalhar em</label>
+          <TaskInput id="tasks" placeholder="Nome do seu projeto"/>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00" 
+            step={5}
+            min={5}
+            max={60}/>
+
+          <span>minutos.</span>
+
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24}/>
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+};
