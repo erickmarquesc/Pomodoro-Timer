1bd1b11b652fbd1ea99225599bd4620c98252bab
Erick Marques <erick.marquesc@hotmail.com>
2024-01-29 09:14:33 -0300

    perf: :art: format of the code

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -11,21 +11,28 @@ export function NewCycleForm() {
   return (
     <FormContainer>
 
-      <label>Vou trabalhar em</label>
+      <label htmlFor="task">Vou trabalhar em</label>
 
       <TaskInput
         id="task"
         disabled={!!activeCycle}
-        placeholder="Nome do seu projeto"
+        placeholder="Nome da tarefa"
+      list="task-suggestions"
         {...register('task')}
       />
+
+      <datalist id="task-suggestions">
+        <option value="Estudar curso" />
+        <option value="Estudar faculdade" />
+        <option value="Ler" />
+      </datalist>
 
       <label htmlFor="minutesAmount">durante</label>
 
       <MinutesAmountInput
         min={1}
         max={60}
-        step={5}
+        /* step={5} */
         type="number"
         placeholder="00"
         id="minutesAmount"
