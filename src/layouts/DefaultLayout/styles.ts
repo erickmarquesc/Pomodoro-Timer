4a9aba7f1c42811500ad92bfa9963179e5613c3a
Erick Marques <erick.marquesc@hotmail.com>
2024-01-30 15:02:10 -0300

    style: :lipstick: update ui

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -4,9 +4,9 @@ export const LayoutContainer = styled.div`
   display: flex;
   padding: 2.5rem;
   max-width: 74rem;
-  margin: 5rem auto;
+  margin: 1.5rem auto;
   border-radius: 8px;
   flex-direction: column;
-  height: calc(100vh - 10rem);
+  height: calc(auto - 10rem);
   background: ${(props)=> props.theme["gray-800"]};
 `;
