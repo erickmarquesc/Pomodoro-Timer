310b364c271eca06b7e00c4ed11e8f5036dd526f
Erick Marques <erick.marquesc@hotmail.com>
2024-02-08 21:46:37 -0300

    perf: :zap: Cria uma nova função com o objetivo de dar funcionalidade aos desafios e faz ajustes de ESLint
    Cria uma nova função para dar aos ciclos os tempos dos desafios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,27 @@
-import { StartCountdownButton, StopCountdownButton, HomeContainer, } from "./styles";
-import { NewCycleForm } from "./components/NewCycleForm";
-import { FormProvider, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Countdown } from "./components/Countdow";
-import { HandPalm, Play } from "phosphor-react";
-import { useContext } from "react";
-import * as zod from "zod";
-import { CyclesContext } from "../../context/CyclesContext";
-import { RangeMessage } from "./components/RangeMessage";
+import { useContext, useEffect, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { HandPalm, Play } from 'phosphor-react'
 
-/* Schema */
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Indique um nome para tarefa'),
-  minutesAmount: zod.number()
-    .min(1, 'O minimo para tarefa é 1 minutos')
-    .max(60, 'O maximo para tarefa é de 60 minutos'),
-});
+import {
+  StartCountdownButton,
+  StopCountdownButton,
+  ChallengeContainer,
+  HomeContainer,
+  FormContent,
+} from './styles'
 
-/* O zod cria a interface necessária pelo schema  */
-type INewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+import { CyclesContext } from '../../context/CyclesContext'
+
+import { INewCycleFormData, newCycleFormValidationSchema } from './zodValidationSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { ChallengeList, IChallengeListProps } from '../../utils/challengeList'
+import { RangeMessage } from './components/RangeMessage'
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdown } from './components/Countdow'
 
 export function Home() {
-  const { createNewCycle,interruptCurrentCycle,activeCycle, cycles} = useContext(CyclesContext);
+  const { createNewCycle, interruptCurrentCycle, activeCycle } = useContext(CyclesContext)
 
   const newCycleForm = useForm<INewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -29,19 +29,31 @@ export function Home() {
       task: '',
       minutesAmount: 0,
     }
-  });
+  })
 
-  const { handleSubmit, watch,  reset, formState:{errors} } = newCycleForm;
+  const { handleSubmit, watch, reset, formState: { errors } } = newCycleForm
 
-  function handleCreateNewCycle(data:INewCycleFormData){
+  function handleCreateNewCycle(data: INewCycleFormData) {
     createNewCycle(data)
-    reset();
-  };
+    reset()
+  }
 
-  const tasks = watch('task');
-  const isSubmitDisabled = !tasks;
+  function handleCreateNewCycleChallenge(data: INewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
+
+  const tasks = watch('task')
+  const isSubmitDisabled = !tasks
 
   const minutesAmountMessageErro = errors.minutesAmount?.message
+
+  const [challenge, setChallenge] = useState<IChallengeListProps>({} as IChallengeListProps)
+
+  useEffect(() => {
+    const randomChallenge = ChallengeList[Math.floor(Math.random() * ChallengeList.length)]
+    setChallenge(randomChallenge)
+  }, [activeCycle])
 
   return (
     <HomeContainer>
@@ -49,33 +61,54 @@ export function Home() {
         action=""
         onSubmit={handleSubmit(handleCreateNewCycle)}
       >
+        <FormContent>
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
 
-        <FormProvider {...newCycleForm}>
-          <NewCycleForm />
-        </FormProvider>
+          <RangeMessage minutesAmountMessageErro={minutesAmountMessageErro} />
 
-        <RangeMessage minutesAmountMessageErro={minutesAmountMessageErro } />
+          <Countdown />
 
-        <Countdown />
+          {activeCycle ? (
+            <StopCountdownButton
+              type="button"
+              onClick={interruptCurrentCycle}
+            >
+              <HandPalm size={24} />
+              Interromper tarefa
+            </StopCountdownButton>
+          ) : (
+            <StartCountdownButton
+              type="submit"
+              disabled={isSubmitDisabled}
+            >
+              <Play size={24} />
+              Começar
+            </StartCountdownButton>)}
+        </FormContent>
 
-        {activeCycle ? (
-          <StopCountdownButton
-            type="button"
-            onClick={interruptCurrentCycle}
-          >
-            <HandPalm size={24} />
-            Interromper tarefa
-          </StopCountdownButton>
-        ) : (
-          <StartCountdownButton
-            type="submit"
-            disabled={isSubmitDisabled}
-          >
-            <Play size={24} />
-            Começar
-          </StartCountdownButton>)}
+        <ChallengeContainer>
+          {challenge && (
+            <>
+              <section>
+                <h1>Tempo {challenge.cycle?.minutesAmount} minutos</h1>
+              </section>
+              <img src={challenge.img} alt="" />
+              <h2>{challenge.title}</h2>
+              <strong>{challenge.description}</strong>
+              <StartCountdownButton
+                type="submit"
+                disabled={!!activeCycle}
+                onClick={() => handleCreateNewCycleChallenge(challenge.cycle)}
+              >
+                Aceitar desafio
+              </StartCountdownButton>
+            </>
+          )}
+        </ChallengeContainer>
 
       </form>
     </HomeContainer >
-  );
-};
+  )
+}
